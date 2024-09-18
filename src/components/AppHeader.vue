b4abac96fd403409150176dfd1eb78350de8eb08
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-09-18 17:44:54 +0200

    added header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,7 +10,14 @@ export default {
 
 <template>
   <header>
-    <h1>Header con Bootstrap</h1>
+    <nav class="navbar bg-warning-subtle">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="../assets/img/yu-gi-oh-logo.png" alt="Logo" width="" height="60" class="d-inline-block align-text-top">
+          <span class="ms-3 fs-3">Yu-Gi-Oh Api</span>
+        </a>
+      </div>
+</nav>
   </header>
 </template>
 
