711b0e155798544f61ecb1a15e8384540e4305f2
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-09-19 14:25:45 +0200

    added loader animations

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card-wrapper">
+    <img class="card" src="../assets/img/card-front.jpg" alt="dragon card front" />
+    <img class="card card-back" src="../assets/img/card-back.jpg" alt="dragon card back" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  // Credits: CHRISTOPHER KADE, link: https://christopherkade.com/posts/css-card-flip
+  @keyframes cardFlip {
+  0% {
+    transform: rotateY(0deg);
+  }
+  50% {
+    transform: rotateY(180deg);
+  }
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+
+.card-wrapper {
+  position: relative;
+  transform-style: preserve-3d;
+  animation: cardFlip 3s ease-in-out infinite;
+  width: 100px;
+  height: 150px;
+}
+
+.card {
+  position: absolute;
+  backface-visibility: hidden;
+  width: 100px;
+  height: 150px;
+}
+
+.card-back {
+  transform: rotateY(180deg);
+}
+
+</style>
